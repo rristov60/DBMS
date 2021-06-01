b468c917df7252b5b2f6ce95eb053f665a55c7df
unknown <risteristov60@yahoo.com>
2021-06-01 20:32:23 +0200

    Second commit

--- Log_In/Program.cs
+++ Log_In/Program.cs
@@ -16,7 +16,7 @@ namespace Log_In
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Log_In_Form());
         }
     }
 }
